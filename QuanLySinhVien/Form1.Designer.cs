661fb273b81c7b53f3450eb60a8a68ed928e0df2
TranTien4121 <trantien04012k1@gmail.com>
2022-10-06 18:12:06 +0700

    update 2

--- QuanLySinhVien/Form1.Designer.cs
+++ QuanLySinhVien/Form1.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.cbblophoc = new System.Windows.Forms.ComboBox();
             this.gridsinhvien = new System.Windows.Forms.DataGridView();
-            this.sinhVienViewModelBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.sinhVienViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.lopHocViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.MaSinhVien = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.HoDem = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ten = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.QueQuan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.iDLopHocDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sinhVienViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sinhVienViewModelBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lopHocViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.gridsinhvien)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lopHocViewModelBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -81,28 +94,11 @@
             this.QueQuan,
             this.iDLopHocDataGridViewTextBoxColumn});
             this.gridsinhvien.DataSource = this.sinhVienViewModelBindingSource;
-            this.gridsinhvien.Location = new System.Drawing.Point(35, 88);
+            this.gridsinhvien.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridsinhvien.Location = new System.Drawing.Point(0, 25);
             this.gridsinhvien.Name = "gridsinhvien";
-            this.gridsinhvien.Size = new System.Drawing.Size(731, 313);
+            this.gridsinhvien.Size = new System.Drawing.Size(771, 299);
             this.gridsinhvien.TabIndex = 2;
-            // 
-            // sinhVienViewModelBindingSource1
-            // 
-            this.sinhVienViewModelBindingSource1.DataSource = typeof(QuanLySinhVien.ViewModel.SinhVienViewModel);
-            // 
-            // sinhVienViewModelBindingSource
-            // 
-            this.sinhVienViewModelBindingSource.DataSource = typeof(QuanLySinhVien.ViewModel.SinhVienViewModel);
-            // 
-            // lopHocViewModelBindingSource
-            // 
-            this.lopHocViewModelBindingSource.DataSource = typeof(QuanLySinhVien.ViewModel.LopHocViewModel);
-            // 
-            // iDDataGridViewTextBoxColumn
-            // 
-            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
-            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
-            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
             // 
             // MaSinhVien
             // 
@@ -140,26 +136,132 @@
             this.QueQuan.HeaderText = "QueQuan";
             this.QueQuan.Name = "QueQuan";
             // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
             // iDLopHocDataGridViewTextBoxColumn
             // 
             this.iDLopHocDataGridViewTextBoxColumn.DataPropertyName = "IDLopHoc";
             this.iDLopHocDataGridViewTextBoxColumn.HeaderText = "IDLopHoc";
             this.iDLopHocDataGridViewTextBoxColumn.Name = "iDLopHocDataGridViewTextBoxColumn";
             // 
+            // sinhVienViewModelBindingSource
+            // 
+            this.sinhVienViewModelBindingSource.DataSource = typeof(QuanLySinhVien.ViewModel.SinhVienViewModel);
+            // 
+            // sinhVienViewModelBindingSource1
+            // 
+            this.sinhVienViewModelBindingSource1.DataSource = typeof(QuanLySinhVien.ViewModel.SinhVienViewModel);
+            // 
+            // lopHocViewModelBindingSource
+            // 
+            this.lopHocViewModelBindingSource.DataSource = typeof(QuanLySinhVien.ViewModel.LopHocViewModel);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.gridsinhvien);
+            this.panel1.Controls.Add(this.statusStrip1);
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Location = new System.Drawing.Point(35, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 346);
+            this.panel1.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripButton3,
+            this.toolStripButton2,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(771, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(112, 22);
+            this.toolStripLabel1.Text = "Danh sách sinh viên";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(57, 22);
+            this.toolStripButton1.Text = "Thêm";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(46, 22);
+            this.toolStripButton2.Text = "Sửa";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(47, 22);
+            this.toolStripButton3.Text = "Xóa";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 324);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(771, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(102, 17);
+            this.toolStripStatusLabel1.Text = "Tổng số sinh viên:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel2.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.gridsinhvien);
+            this.ClientSize = new System.Drawing.Size(845, 453);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.cbblophoc);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.gridsinhvien)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sinhVienViewModelBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.lopHocViewModelBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,6 +289,15 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
         private System.Windows.Forms.DataGridViewTextBoxColumn QueQuan;
         private System.Windows.Forms.DataGridViewTextBoxColumn iDLopHocDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
     }
 }
 
